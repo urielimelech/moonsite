3eb21b5960b48c1009631f403d15413648b738b5
urielimelech <urielimelech@hotmail.com>
2021-04-23 13:20:23 +0300

    add data object to clothes and debugger

---
+++ src/DataObjects/cloth.js
@@ -0,0 +1,10 @@
+export default class Cloth {
+    constructor(cloth) {
+        this.id = cloth.id
+        this.type = cloth.type
+        this.name = cloth.name
+        this.colors = cloth.colors
+        this.sizes = cloth.sizes
+        this.brand = cloth.brand
+    }
+}
---
+++ src/viewableConsts.js
@@ -0,0 +1,3 @@
+const API_URL = 'http://www.mocky.io/v2/5e3940013200005e00ddf87e?mocky-delay=600ms'
+
+export { API_URL }
